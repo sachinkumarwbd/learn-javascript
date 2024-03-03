6e18d535ff9dbad5a11aaa7fd10eb397d8d63584
sachinkumarwbd <srpgkumaar1234@gmail.com>
2024-03-03 12:18:24 +0530

    learn

---
+++ datatypes/script.js
@@ -0,0 +1,55 @@
+// primitive data types
+
+//numbers
+
+let age = 1223;
+
+// srting
+
+let name = "sachin";
+let email = "sachin.com";
+
+// Boolean
+
+let login = true;
+let logout = false;
+
+// null
+
+let val = null;
+
+// undefined value not assighn
+
+let id;
+
+// BigInt big value
+
+let num = 3838783479888;
+
+// Symbol  uniqe
+
+// nonprimitive data types
+
+// Object array function
+
+let obj = {
+  name: "sachin",
+  age: 36,
+  email: "sachin.com",
+};
+console.log(obj);
+
+// array
+
+let array = [2, 3, 4, 5, 5, 6, 67, 7];
+
+console.log(array);
+
+// function
+function person() {
+  let arr = [2, 3, 4, 5, 5, 6, 67, 7];
+
+  console.log(arr);
+}
+
+person();
---
+++ variables/script.js
@@ -0,0 +1,21 @@
+// globle scope
+
+var password = 1276474648; //always change value
+
+let email = "acoumtgamil"; //block scope
+
+// let email = "google.com";
+
+console.log(email);
+
+const id = 28363; //not change value
+console.log(id);
+id = 68876566;
+
+console.log(id);
+
+password = 6775;
+
+console.log(password);
+
+console.table([password, id]);
