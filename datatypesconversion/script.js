2aca2cf3fc72a201c1ad621352dd3e66ef1f5ec4
sachinkumarwbd <srpgkumaar1234@gmail.com>
2024-03-03 12:41:11 +0530

    conversion

---
+++ datatypesconversion/script.js
@@ -0,0 +1,15 @@
+// "" = false
+// 0 = false
+// 1 = true
+// "sachin" true
+
+// "5677sacvb" = NaN
+
+// type check
+
+let name = "sachin";
+let age = 28;
+let id;
+let val = null;
+
+console.log(typeof age, typeof name);
